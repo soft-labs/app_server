fd63282c5bbb82e47a826741e72529b577a58bb6
labs20 <labs@softlabs.com.br>
2016-06-14 14:31:41 -0300

    Componente template implementado

--- business_objects/dbms/contabil/cont_historicos/cont_historicos.js
+++ business_objects/dbms/contabil/cont_historicos/cont_historicos.js
@@ -78,12 +78,13 @@ function ContHistoricos(){
             linhas: [
                 {titulo: "Informações da Categoria de Lançamento"},
                 {ativo: 9, historico: 50, cont_determinacao_key: 30,tipo: 15,},
-                {observacoes: 100}
+                {template: 100}
             ],
             ctrls: {
-                historico: {
-                    extra_right: { class: '', tag: '' },
-                    extra_left:  { class: '', tag: '' }
+                template: {
+                    id: 'teste-labs',
+                    html:  '<b>OI</b>',
+                    tipo: types.comp.template
                 }
             }
         }
